af2a93a427add2d06d3e26c0eee7dd17343fd34d
Marcio Ubiratan Rosa <mubiratan@gmail.com>
2026-02-02 18:19:21 -0300

    add number partitions

--- src/main/java/br/com/btgpactual/config/KafkaConfig.java
+++ src/main/java/br/com/btgpactual/config/KafkaConfig.java
@@ -1,6 +1,7 @@
 package br.com.btgpactual.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
@@ -8,6 +9,9 @@ import org.springframework.kafka.config.TopicBuilder;
 @Configuration
 public class KafkaConfig {
     public static final String ORDER_CREATED_TOPIC = "btg-pactual-order.created";
+
+    @Value("${spring.kafka.topic.partitions:1}")
+    private int partitions;
 
 //    @Bean
 //    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
@@ -18,7 +22,7 @@ public class KafkaConfig {
     @Bean
     public NewTopic orderCreatedTopic() {
         return TopicBuilder.name(ORDER_CREATED_TOPIC)
-                .partitions(1)
+                .partitions(partitions)
                 .replicas(1)
                 .build();
     }
--- src/main/java/br/com/btgpactual/listener/OrderCreatedListener.java
+++ src/main/java/br/com/btgpactual/listener/OrderCreatedListener.java
@@ -5,7 +5,9 @@ import br.com.btgpactual.service.OrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 import static br.com.btgpactual.config.KafkaConfig.ORDER_CREATED_TOPIC;
@@ -22,8 +24,8 @@ public class OrderCreatedListener {
     }
 
     @KafkaListener(topics = ORDER_CREATED_TOPIC)
-    public void listen(Message<OrderCreatedEvent> message) {
-        logger.info("Message consumed: {}", message.getPayload());
+    public void listen(Message<OrderCreatedEvent> message, @Header(KafkaHeaders.RECEIVED_PARTITION) int partition) {
+        logger.info("Message consumed from partition {}: {}", partition, message.getPayload());
         orderService.save(message.getPayload());
     }
 }
